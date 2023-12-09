fb5b2afe548329d579745e99fd87cb17d443456e
NathamFernandes <natham.fr@gmail.com>
2023-12-09 11:23:00 -0300

    removendo funcao inutulizada no header de searches

--- modules/headers/searches.h
+++ modules/headers/searches.h
@@ -2,8 +2,6 @@
 
 void search_main_caller(Table table, int column_option, order search_option);
 int number_is_equal_less_or_greater(double user_number);
-void search_number();
-void search_string();
 char* get_order_name(order ord);
 order compare(char value_one[200], char value_two[200], types type);
 Table filter_table(Table table, int column_option, char value[200], order filter_order);
